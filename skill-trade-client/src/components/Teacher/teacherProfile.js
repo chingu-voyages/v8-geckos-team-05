04f92fa8dc0a7bd7e06be58d859859b4023832f0
JeffVega <jeff.vega321@gmail.com>
2019-03-20 15:53:53 -0700

    installed sass

---
+++ skill-trade-client/src/components/Teacher/teacherProfile.js
@@ -0,0 +1,11 @@
+ import React, { Component } from 'react'
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Team</h1>
+      </div>
+    )
+  }
+}
