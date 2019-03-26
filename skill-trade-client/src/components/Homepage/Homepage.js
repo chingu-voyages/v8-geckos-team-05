8a8371f923a65c76d9a788a4e41125c2c2f7d532
Chris Fotso <chrisfotso@gmail.com>
2019-03-26 17:20:08 -0500

    Filled out basic homepage skeleton

--- skill-trade-client/src/components/Homepage/Homepage.js
+++ skill-trade-client/src/components/Homepage/Homepage.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Homepage = () => {
   return (
-    <div>
-      
+    <div id="homepage-main">
+      <div id="homepage-text-container">
+        <div id="large-text-container">
+          <h1 id="homepage-main-text">Talent Trade</h1>
+          <h2 id="homepage-sub-text">Practice makes perfect.</h2>
+        </div>
+        <div id="small-text-container">
+          <p className="homepage-small-text">
+            Learn a new talent easily, picking from one of many teachers — or,
+            share one of your own talents with one of many eager students.
+          </p>
+          <p className="homepage-small-text strong">
+            Your choice.
+            <br />
+            Your skills.
+            <br />
+            Your way.
+            <br />
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
