5fa185b20bdb22ba1bb58983fa56f3c553d43fd9
Chris Fotso <chrisfotso@gmail.com>
2019-04-03 17:59:45 -0500

    fixed issue with footer floating in the middle of the page

--- skill-trade-client/src/components/Homepage/Homepage.js
+++ skill-trade-client/src/components/Homepage/Homepage.js
@@ -2,32 +2,42 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div className="homepage">
-      <div className="homepage__intro">
-        <div className="homepage__intro--textContainer">
-          <h1 className="homepage__intro--mainText">Talent Trade</h1>
-          <h2 className="homepage__intro--subtext">Practice makes perfect.</h2>
+    <>
+      <div className="homepage">
+        <div className="homepage__intro">
+          <div className="homepage__intro--textContainer">
+            <h1 className="homepage__intro--mainText">Talent Trade</h1>
+            <h2 className="homepage__intro--subtext">
+              Practice makes perfect.
+            </h2>
+          </div>
+          <div className="homepage__intro--button--container">
+            <button className="homepage__intro--button rounded-button">
+              Log In or Sign Up
+            </button>
+          </div>
         </div>
-        <div className="homepage__intro--button--container">
-          <button className="homepage__intro--button rounded-button">
-            Log In or Sign Up
-          </button>
-        </div>
-      </div>
-      <div className="homepage__info">
-        <div className="homepage__info--column" id="homepage__info--column1">
-        <h2 className="homepage__info--column--header">
-            Learn a new skill...
-          </h2>
-        </div>
+        <div className="homepage__info">
+          <div className="homepage__info--column" id="homepage__info--column1">
+            <h2 className="homepage__info--column--header">
+              Learn a new skill...
+            </h2>
+          </div>
 
-        <div className="homepage__info--column" id="homepage__info--column2">
-          <h2 className="homepage__info--column--header">
-            ...or share one of your own.
-          </h2>
+          <div className="homepage__info--column" id="homepage__info--column2">
+            <h2 className="homepage__info--column--header">
+              ...or share one of your own.
+            </h2>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="homepage__footer">
+        <p className="homepage__footer--text">
+          Created by <strong>Jeff</strong>, <strong>Eliezer</strong>, and{" "}
+          <strong>Chris</strong> for the Chingu Voyage-08
+        </p>
+      </div>
+    </>
   );
 };
 
