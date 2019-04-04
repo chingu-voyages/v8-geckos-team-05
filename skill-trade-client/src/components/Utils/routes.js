9390a02a88c5faf173eca411edee7cfc289356ad
Chris Fotso <chrisfotso@gmail.com>
2019-04-04 17:55:19 -0500

    added router to homepage component

--- skill-trade-client/src/components/Utils/routes.js
+++ skill-trade-client/src/components/Utils/routes.js
@@ -8,12 +8,14 @@ import {
 import contactsMain from "../Contacts/contactsMain.jsx";
 import teacherProfile from "../Teacher/teacherProfile";
 import teacherSearch from "../Teacher/teacherSch";
+import Homepage from '../Homepage/Homepage';
 export default function Index() {
   return (
     <Router>
       <div>
         <Switch>
           <Redirect exact from="/" to="/home" />
+          <Route exact path="/home" component={Homepage} />
           <Route exact path="/contacts" component={contactsMain} />
           <Route exact path="/teacherProfile" component={teacherProfile} />
           <Route exact path="/teacherSearch" component={teacherSearch} />
