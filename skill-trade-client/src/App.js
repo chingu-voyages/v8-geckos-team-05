aeda31eb87ef26e023b9ce899edfea1921bd7298
Chris Fotso <chrisfotso@gmail.com>
2019-04-04 17:29:04 -0500

    slight changes to app.js

--- skill-trade-client/src/App.js
+++ skill-trade-client/src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import TeachProfile from "./components/Teacher/teacherProfile";
-import ContactsPg from "./components/Contacts/contactsMain";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TeachProfile />
-        <ContactsPg />
       </div>
     );
   }
