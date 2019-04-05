fc621b31edb213a024b2095c43976cab96d3db31
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-05 14:16:05 -0700

    added mainContainer component and its styling files and imports

---
+++ skill-trade-client/src/components/MainContainer/mainContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class MainContainer extends Component {
+  state = {};
+  render() {
+    return <div className="mainContainer" />;
+  }
+}
+
+export default MainContainer;
