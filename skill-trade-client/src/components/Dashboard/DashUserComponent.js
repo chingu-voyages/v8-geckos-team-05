0289a6fc7c762291a5d5007b74dabcc59ce52c7e
Chris Fotso <chrisfotso@gmail.com>
2019-04-05 19:54:15 -0500

    Created User Component for Dashboard

---
+++ skill-trade-client/src/components/Dashboard/DashUserComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DashUserComponent = () => {
+  return (
+    <div className="dashboard__userComponent">
+        <div className="dashboard__userComponent--imageContainer">
+            <img src="./dashboardProfilePicture.png" alt="User's profile picture" srcset="" className="dashboard__userComponent--image"/>
+        </div>
+        <p className="dashboard__userComponent--username"><strong>Chris</strong></p>
+    </div>
+  )
+}
+
+export default DashUserComponent
--- skill-trade-client/src/components/Dashboard/DashboardPage.js
+++ skill-trade-client/src/components/Dashboard/DashboardPage.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+import DashUserComponent from './DashUserComponent';
+
+import Navbar from "../Navbar/navBar";
 
 class Dashboard extends Component {
+  // 5 columns, 8 rows
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <>
+        <Navbar/>
+        <div className="dashboard">
+          <DashUserComponent/>
+        </div>
+      </>
+    );
   }
 }
 
-export default Dashboard
+export default Dashboard;
