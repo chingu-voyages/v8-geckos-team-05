01b255039eefe75385c0ca36255b4cd7357b40ad
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-08 12:15:48 -0700

    added routing

--- skill-trade-client/src/components/Navbar/navBar.js
+++ skill-trade-client/src/components/Navbar/navBar.js
@@ -1,46 +1,56 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+  Link
+} from "react-router-dom";
+import Homepage from "../Homepage/Homepage";
+import teacherSearch from "../Teacher/teacherSch";
 class NavBar extends Component {
   state = {};
   render() {
     return (
       <>
         <nav className="navbar">
-          <a className="navbar__banner" href="#">
+          <Link to="/home" className="navbar__banner" href="#">
             TALENT TRADE
-          </a>
-          {/* <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button> */}
-          <div className="navbar__collapse">
-            <ul className="listedItems">
-              <li className="listedItems__item">
-                <a className="listedItems__item__link" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="listedItems__item">
-                <a className="listedItems__item__link" href="#">
-                  Teachers
-                </a>
-              </li>
-              <li className="listedItems__item">
-                <a className="listedItems__item__link" href="#">
-                  Search
-                </a>
-              </li>
-            </ul>
-            <button className="navbar__btn-logout" type="submit">
-              Log Out
-            </button>
-          </div>
+          </Link>
+          <Switch>
+            <div className="navbar__collapse">
+              <ul className="listedItems">
+                <li className="listedItems__item">
+                  <Link to="/home" className="listedItems__item__link" href="#">
+                    Home
+                  </Link>
+                </li>
+                <li className="listedItems__item">
+                  <Link
+                    to="/teachersearch"
+                    className="listedItems__item__link"
+                    href="#"
+                  >
+                    Teachers
+                  </Link>
+                </li>
+                <li className="listedItems__item">
+                  <Link className="listedItems__item__link" href="#">
+                    Search
+                  </Link>
+                </li>
+              </ul>
+              <button className="navbar__btn-logout" type="submit">
+                Log Out
+              </button>
+            </div>
+            <Route path="/home" exact={true} component={Homepage} />
+            <Route
+              path="/teacherSearch"
+              exact={true}
+              component={teacherSearch}
+            />
+          </Switch>
         </nav>
       </>
     );
