20707ce9717d64e23ca894e2dcaba679a456926b
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-09 14:37:08 -0700

    deleted switch

--- skill-trade-client/src/components/Navbar/navBar.js
+++ skill-trade-client/src/components/Navbar/navBar.js
@@ -23,38 +23,30 @@ class NavBar extends Component {
             </span>
           </div>
           <div className="navbar__collapse--items show">
-            <Switch>
-              <ul className="listedItems">
-                <li className="listedItems__item">
-                  <Link to="/home" className="listedItems__item__link" href="#">
-                    Home
-                  </Link>
-                </li>
-                <li className="listedItems__item">
-                  <Link
-                    to="/teachersearch"
-                    className="listedItems__item__link"
-                    href="#"
-                  >
-                    Teachers
-                  </Link>
-                </li>
-                <li className="listedItems__item">
-                  <Link className="listedItems__item__link" href="#">
-                    Search
-                  </Link>
-                </li>
-              </ul>
-              <button className="navbar__btn-logout" type="submit">
-                Log Out
-              </button>
-              <Route path="/home" exact={true} component={Homepage} />
-              <Route
-                path="/teacherSearch"
-                exact={true}
-                component={teacherSearch}
-              />
-            </Switch>
+            <ul className="listedItems">
+              <li className="listedItems__item">
+                <Link to="/home" className="listedItems__item__link" href="#">
+                  Home
+                </Link>
+              </li>
+              <li className="listedItems__item">
+                <Link
+                  to="/teachersearch"
+                  className="listedItems__item__link"
+                  href="#"
+                >
+                  Teachers
+                </Link>
+              </li>
+              <li className="listedItems__item">
+                <Link className="listedItems__item__link" href="#">
+                  Search
+                </Link>
+              </li>
+            </ul>
+            <button className="navbar__btn-logout" type="submit">
+              Log Out
+            </button>
           </div>
         </nav>
       </>
