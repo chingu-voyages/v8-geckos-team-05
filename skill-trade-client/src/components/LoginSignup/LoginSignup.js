221c40e467dbbce660fe67e10977b61980775199
Chris Fotso <chrisfotso@gmail.com>
2019-04-10 15:28:36 -0500

    created login and signup component

---
+++ skill-trade-client/src/components/LoginSignup/LoginSignup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LoginSignup extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default LoginSignup
