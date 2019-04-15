3a6597379ebbb463ad92c98431c0284c11117c42
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-15 14:50:17 -0700

    finished the dropdown on the searchBar

--- skill-trade-client/src/components/Contacts/contactsMain.js
+++ skill-trade-client/src/components/Contacts/contactsMain.js
@@ -6,6 +6,38 @@ import MainContainer from "../MainContainer/mainContainer";
 class ContactsMain extends Component {
   state = {};
   render() {
+    const handleDropdown = () => {
+      const topBar = document.querySelector(".contentBox--contacts__topBar");
+      topBar.classList.toggle("contentBox--contacts__topBar--responsive");
+
+      const topBar__myMentors = document.querySelector(
+        ".contentBox--contacts__topBar__myMentors"
+      );
+      topBar__myMentors.classList.toggle(
+        "contentBox--contacts__topBar__myMentors--responsive"
+      );
+
+      const topBar__myMentees = document.querySelector(
+        ".contentBox--contacts__topBar__myMentees"
+      );
+      topBar__myMentees.classList.toggle(
+        "contentBox--contacts__topBar__myMentees--responsive"
+      );
+
+      const topBar__myClassmates = document.querySelector(
+        ".contentBox--contacts__topBar__myClassmates"
+      );
+      topBar__myClassmates.classList.toggle(
+        "contentBox--contacts__topBar__myClassmates--responsive"
+      );
+
+      const topBar__showAll = document.querySelector(
+        ".contentBox--contacts__topBar__showAll"
+      );
+      topBar__showAll.classList.toggle(
+        "contentBox--contacts__topBar__showAll--responsive"
+      );
+    };
     return (
       <>
         <NavBar />
@@ -27,11 +59,12 @@ class ContactsMain extends Component {
               </div>
             </div>
             <div className="contentBox--contacts__searchBar">
-              <div className="contentBox--contacts__searchBar__collapseBtn">
-                <h2>
-                  <i className="fas fa-bars" />
-                </h2>
-              </div>
+              <button
+                onClick={handleDropdown}
+                className="contentBox--contacts__searchBar__btnCollapse"
+              >
+                <i className="fas fa-bars" />
+              </button>
               <input
                 className="contentBox--contacts__searchBar__search"
                 type="search"
