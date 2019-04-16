e47d36ce7f043afac54cb7c89dd3c6cbe7452c52
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-16 14:20:53 -0700

    added img element to the imgContainer

--- skill-trade-client/src/components/Contacts/friendCard.js
+++ skill-trade-client/src/components/Contacts/friendCard.js
@@ -4,7 +4,9 @@ class FriendCard extends Component {
   render() {
     return (
       <div className="mainContainerCard">
-        <div className="mainContainerCard__imgContainer" />
+        <div className="mainContainerCard__imgContainer">
+          <img className="mainContainerCard__imgContainer__img" />
+        </div>
         <div className="mainContainerCard__textContainer">
           <p className="mainContainerCard__textContainer__name">Name</p>
           <p className="mainContainerCard__textContainer__teachingProgram">
