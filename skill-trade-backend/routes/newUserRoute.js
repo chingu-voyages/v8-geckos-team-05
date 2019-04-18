8531864f7d6add15770202a4e2a4486cc163f428
Chris Fotso <chrisfotso@gmail.com>
2019-04-18 15:06:12 -0500

    Used Prettier to properly format code

--- skill-trade-backend/routes/newUserRoute.js
+++ skill-trade-backend/routes/newUserRoute.js
@@ -3,69 +3,74 @@ const router = express.Router();
 const User = require("../models/user");
 
 router.post("/register", async (req, res, next) => {
-  let { username, password } = req.body;
+    let {
+        username,
+        password
+    } = req.body;
 
-  username = username.toLowerCase();
+    username = username.toLowerCase();
 
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ err: "Please enter a username and password" });
-  }
-
-  const hasWhiteSpace = str => /.*\s.*/.test(str);
-
-  const isNotString = str => typeof str !== "string";
-
-  const usernameExistsInDb = async username => {
-    const existingUser = await User.findOne({
-      username
-    });
-
-    if (existingUser) {
-      return true;
-    } else return false;
-  };
-
-  try {
-    if ([username, password].some(el => hasWhiteSpace(el))) {
-      return res.status(422).json({
-        err: "Entries cannot contain whitespace"
-      });
-    } else if ([username, password].some(el => isNotString(el))) {
-      return res.status(422).json({
-        err: "Entries must be strings"
-      });
-    } else if (password.length < 8 || password.length > 72) {
-      return res.status(422).json({
-        err: "Password must be between 8 and 72 characters in length"
-      });
-    } else if (await usernameExistsInDb(username)) {
-      return res.status(400).json({
-        err: "Username already exists"
-      });
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({
+                err: "Please enter a username and password"
+            });
     }
 
-    const hashedPassword = await User.hashPassword(password);
+    const hasWhiteSpace = str => /.*\s.*/.test(str);
 
-    const newUser = {
-      username,
-      password: hashedPassword,
-      friends: [],
-      online: null
+    const isNotString = str => typeof str !== "string";
+
+    const usernameExistsInDb = async username => {
+        const existingUser = await User.findOne({
+            username
+        }).exec();
+
+        if (existingUser) {
+            return true;
+        } else return false;
     };
 
-    const savedUser = await User.create(newUser);
+    try {
+        if ([username, password].some(el => hasWhiteSpace(el))) {
+            return res.status(422).json({
+                err: "Entries cannot contain whitespace"
+            });
+        } else if ([username, password].some(el => isNotString(el))) {
+            return res.status(422).json({
+                err: "Entries must be strings"
+            });
+        } else if (password.length < 8 || password.length > 72) {
+            return res.status(422).json({
+                err: "Password must be between 8 and 72 characters in length"
+            });
+        } else if (await usernameExistsInDb(username)) {
+            return res.status(400).json({
+                err: "Username already exists"
+            });
+        }
 
-    return res
-      .status(201)
-      .location(`${req.originalUrl}/${savedUser.id}`)
-      .json(savedUser);
-  } catch (err) {
-    return res.status(400).json({
-      err: err
-    });
-  }
+        const hashedPassword = await User.hashPassword(password);
+
+        const newUser = {
+            username,
+            password: hashedPassword,
+            friends: [],
+            online: null
+        };
+
+        const savedUser = await User.create(newUser);
+
+        return res
+            .status(201)
+            .location(`${req.originalUrl}/${savedUser.id}`)
+            .json(savedUser);
+    } catch (err) {
+        return res.status(400).json({
+            err: err
+        });
+    }
 });
 
 module.exports = router;
