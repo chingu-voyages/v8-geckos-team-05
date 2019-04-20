e9b0439b47d589d10de4b82b511613f6aa630a61
Chris Fotso <chrisfotso@gmail.com>
2019-04-19 20:10:39 -0500

    Added functionality to accept friend requests

--- skill-trade-backend/routes/functions/friends.js
+++ skill-trade-backend/routes/functions/friends.js
@@ -1,4 +1,5 @@
 const User = require('../../models/user')
+const Friend = require('../../models/friend')
 
 const checkIfRequestAlreadySent = async (requesterId, recipientId) => {
     const userDoc = await User
@@ -6,7 +7,9 @@ const checkIfRequestAlreadySent = async (requesterId, recipientId) => {
         .populate('friendRequests', 'requester')
         .exec();
 
-    return userDoc.friendRequests.some(({ requester }) => requester.toString() === requesterId)
+    return userDoc.friendRequests.some(({
+        requester
+    }) => requester.toString() === requesterId)
 }
 
 const sendFriendRequestToUser = async (userId, reqId) => {
@@ -27,7 +30,74 @@ const sendFriendRequestToUser = async (userId, reqId) => {
     return updatedUser
 }
 
+const acceptFriendRequest = async friendRequestId => {
+    const friendRequest = await Friend.findById(friendRequestId).exec();
+
+    const {
+        requester: requesterId,
+        recipient: recipientId
+    } = friendRequest;
+
+    await deleteFriendRequest(recipientId, friendRequestId);
+
+    const [updatedRequester, updatedRecipient] = await Promise.all(
+        [
+            pushFriendToUser(requesterId, recipientId),
+            pushFriendToUser(recipientId, requesterId)
+        ]
+    )
+
+    return {
+        updatedRequester,
+        updatedRecipient
+    }
+}
+
+const denyFriendRequest = async friendRequestId => {
+    const friendRequest = await Friend.findById(friendRequestId).exec();
+
+    const {
+        recipient: recipientId
+    } = friendRequest;
+
+    const updatedRecipient = await deleteFriendRequest(recipientId, friendRequestId);
+
+    return updatedRecipient;
+}
+
+const pushFriendToUser = async (userId, friendId) => {
+    const userWithAddedFriend = await User
+        .findByIdAndUpdate(
+            userId, {
+                $push: {
+                    friends: friendId
+                }
+            }, {
+                new: true,
+                select: 'username friends friendRequests'
+            }
+        )
+        .populate('friends', 'username')
+        .exec();
+
+    return userWithAddedFriend;
+}
+
+const deleteFriendRequest = async (userId, requestId) => {
+    const updatedUser = await User.findByIdAndUpdate(
+        userId, {
+            $pull: {
+                friendRequests: requestId
+            }
+        }
+    ).exec();
+
+    return updatedUser;
+}
+
 module.exports = {
     checkIfRequestAlreadySent,
-    sendFriendRequestToUser
+    sendFriendRequestToUser,
+    acceptFriendRequest,
+    denyFriendRequest
 }
--- skill-trade-backend/routes/newFriendsRoute.js
+++ skill-trade-backend/routes/newFriendsRoute.js
@@ -1,44 +1,70 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Friend = require('../models/friend');
-const User = require('../models/user');
+const Friend = require("../models/friend");
+const User = require("../models/user");
 
 const {
-    checkIfRequestAlreadySent,
-    sendFriendRequestToUser
-} = require('./functions/friends')
+  checkIfRequestAlreadySent,
+  sendFriendRequestToUser,
+  acceptFriendRequest,
+  denyFriendRequest
+} = require("./functions/friends");
 
-router.post('/friend_request/:id', async (req, res, next) => {
-    const recipientId = req.params.id;
-    const requesterId = req.body.requesterId;
+router.post("/friend_request/send/:id", async (req, res) => {
+  const recipientId = req.params.id;
+  const requesterId = req.body.requesterId;
 
-    const newFriend = {
-        requester: requesterId,
-        recipient: recipientId,
-        status: null
-    }
+  const newFriend = {
+    requester: requesterId,
+    recipient: recipientId,
+    status: null
+  };
 
-    if (await checkIfRequestAlreadySent(requesterId, recipientId)) {
-        return res.status(400).send('User already has friend');
-    }
+  if (await checkIfRequestAlreadySent(requesterId, recipientId)) {
+    return res.status(400).send("User already has friend");
+  }
 
-    try {
-        const friendRequest = await Friend.create(newFriend);
-        const friendRequestId = friendRequest.id;
+  try {
+    const friendRequest = await Friend.create(newFriend);
+    const friendRequestId = friendRequest.id;
 
-        const addedFriend = await sendFriendRequestToUser(recipientId, friendRequestId);
+    const addedFriend = await sendFriendRequestToUser(
+      recipientId,
+      friendRequestId
+    );
 
-        return res.status(201).json({
-            msg: 'Success: friend request sent',
-            newFriend: addedFriend
-        })
-    } catch (err) {
-        return res.status(500).json({
-            msg: 'Unknown error occured',
-            err
-        })
-    }
-})
+    return res.status(201).json({
+      msg: "Success: friend request sent",
+      newFriend: addedFriend
+    });
+  } catch (err) {
+    return res.status(500).json({
+      msg: "Unknown error occured",
+      err
+    });
+  }
+});
+
+router.post("/friend_request/respond", async (req, res) => {
+  const { id: friendRequestId, accept: userResponse } = req.query;
+
+  if (userResponse === 'true') {
+    const { updatedRequester, updatedRecipient } = await acceptFriendRequest(friendRequestId);
+    
+    return res.status(201).json({
+        msg: "Success: friend added, request deleted from recipient's document",
+        updatedRequester,
+        updatedRecipient
+    })
+  } else {
+    const { updatedRecipient } = await denyFriendRequest({friendRequestId});
+    
+    return res.status(200).json({
+        msg: "Success: Friend request denied, request deleted from recipient's document",
+        updatedRecipient
+    })
+  }
+});
 
 module.exports = router;
