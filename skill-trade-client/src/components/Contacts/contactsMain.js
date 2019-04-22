b08ba4103031d93a4bc98dc7c10bcea0ac25b826
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-22 14:16:26 -0700

    improved height of the container

--- skill-trade-client/src/components/Contacts/contactsMain.js
+++ skill-trade-client/src/components/Contacts/contactsMain.js
@@ -46,7 +46,7 @@ class ContactsMain extends Component {
     const handleShowAll = () => {
       let results = 30; //to be modified according to this.state.all.length
       fetch(
-        `https://api.unsplash.com/photos/random/?query=hot+girl&featured&count=${results}&client_id=1de021314b1360d29ebe9ac43c8388233b18f1aadd026e729e84a9fb355b6b46`
+        `https://api.unsplash.com/photos/random/?query=girl&featured&count=${results}&client_id=1de021314b1360d29ebe9ac43c8388233b18f1aadd026e729e84a9fb355b6b46`
       )
         .then(res => res.json())
         .then(data => {
