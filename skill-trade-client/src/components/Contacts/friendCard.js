2be9aca0bd830827294fd2114520608401758c2e
Eliezer Valenzuela <vzla0094@gmail.com>
2019-04-22 12:07:16 -0700

    making adjustments to insert images using the unspash api

--- skill-trade-client/src/components/Contacts/friendCard.js
+++ skill-trade-client/src/components/Contacts/friendCard.js
@@ -4,7 +4,14 @@ class FriendCard extends Component {
   render() {
     return (
       <div className="mainContainerCard">
-        <div className="mainContainerCard__imgContainer" />
+        <div
+          className="mainContainerCard__imgContainer"
+          style={{
+            backgroundImage: `url(
+              "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+            )`
+          }}
+        />
         <div className="mainContainerCard__textContainer">
           <p className="mainContainerCard__textContainer__name">Name</p>
           <p className="mainContainerCard__textContainer__teachingProgram">
