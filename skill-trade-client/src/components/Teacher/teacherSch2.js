844d9c74c8ea9d291923a97721fa81ac1a457251
JeffVega <jeff.vega321@gmail.com>
2019-04-24 19:29:03 -0700

    refactor code & updated type selector

--- skill-trade-client/src/components/Teacher/teacherSch2.js
+++ skill-trade-client/src/components/Teacher/teacherSch2.js
@@ -5,21 +5,32 @@ import MainContainer from "../MainContainer/mainContainer";
 
 class TeacherSearch2 extends Component {
   state = {
-    all: []
+    all: [],
+    type:""
   };
+  typeOfActivity(event){
+    this.setState({
+      type:event.target.value
+    })
+
+  }
+
+  handlePictureSearch(e){
+    let limitList = 30;
+    let typeOfPicture = this.state.type;
+    e.preventDefault();
+    fetch(`https://api.unsplash.com/photos/random/?query=${typeOfPicture}&featured&count=${limitList}&client_id=1de021314b1360d29ebe9ac43c8388233b18f1aadd026e729e84a9fb355b6b46`)
+    .then(res => res.json())
+    .then( data =>{
+      this.setState({
+        all:data
+      })
+  
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
-    const handleSearch = e => {
-      e.preventDefault();
-      let results = 30; //to be modified according to this.state.all.length
-      fetch(
-        `https://api.unsplash.com/photos/random/?query=girl&featured&count=${results}&client_id=1de021314b1360d29ebe9ac43c8388233b18f1aadd026e729e84a9fb355b6b46`
-      )
-        .then(res => res.json())
-        .then(data => {
-          this.setState({ all: data });
-          console.log(this.state);
-        });
-    };
     return (
       <>
         <NavBar />
@@ -27,8 +38,9 @@ class TeacherSearch2 extends Component {
           <h1 className="mainContainer__title">Teacher Search</h1>
           <div className="contentBox--teacherSearch">
             <div className="contentBox--teacherSearch__searchBar">
-              <form onSubmit={handleSearch}>
+              <form onSubmit={(e)=> this.handlePictureSearch(e)}>
                 <select
+                  onChange={this.typeOfActivity.bind(this)}
                   className="contentBox--teacherSearch__searchBar__dropdown"
                   required
                 >
@@ -39,7 +51,7 @@ class TeacherSearch2 extends Component {
                   <option value="Spanish">Spanish</option>
                   <option value="English">English</option>
                   <option value="Piano">Piano</option>
-                  <option value="Other">Other</option>
+                  <option value="Profile Picture">Other</option>
                 </select>
                 <input
                   type="submit"
